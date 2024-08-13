d898f5925db0063d6523b825782b2f5e1c915d72
Vaulkryn <vaulkryn@outlook.fr>
2024-08-13 23:27:10 +0200

    formatted numbers with tofixed method

--- src/config/EnemyAttacker.js
+++ src/config/EnemyAttacker.js
@@ -1,28 +1,28 @@
 const enemyT1v1 = {
     core: {
-        scaleFactor: 2,
-        origin: { x: 187.29513549804688, y: 185.77508544921875 },
+        scaleFactor: 2.1,
+        origin: { x: 187.295, y: 185.775 },
         offset: { x: 0, y: 0 },
         fillStyle: 'rgba(45, 45, 45, 1)'
     },
     top: {
-        scaleFactor: 2,
-        origin: { x: 178.54525756835938, y: 134 },
+        scaleFactor: 2.1,
+        origin: { x: 178.545, y: 134 },
         offset: { x: 0, y: 0 },
         fillStyle: 'rgba(255, 255, 255, 1)'
     },
     left: {
-        scaleFactor: 2,
-        origin: { x: 214.46133422851562, y: 228.59820556640625 },
-        rotation: -2.1622128102625267,
+        scaleFactor: 2.1,
+        origin: { x: 214.461, y: 228.598 },
+        rotation: -2.1622,
         offset: { x: -45, y: -23 },
         scaleY: -1,
         fillStyle: 'rgba(255, 255, 255, 1)'
     },
     right: {
-        scaleFactor: 2,
-        origin: { x: 194.53866386413574, y: 228.59815216064453 },
-        rotation: -0.9793798433272664,
+        scaleFactor: 2.1,
+        origin: { x: 194.539, y: 228.598 },
+        rotation: -0.9794,
         offset: { x: -25, y: -23 },
         fillStyle: 'rgba(255, 255, 255, 1)'
     }
--- src/config/PlayerCharacter.js
+++ src/config/PlayerCharacter.js
@@ -1,28 +1,28 @@
 const basicPlayer = {
     core: {
         scaleFactor: 3.5,
-        origin: { x: 413.5226745605469, y: 349.4304504394531 },
+        origin: { x: 413.523, y: 349.43 },
         offset: { x: 0, y: 0 },
         fillStyle: 'rgba(45, 45, 45, 1)'
     },
     top: {
         scaleFactor: 3.5,
-        origin: { x: 398.75115966796875, y: 210 },
+        origin: { x: 398.751, y: 210 },
         offset: { x: 0, y: 0 },
         fillStyle: 'rgba(255, 255, 255, 1)'
     },
     left: {
         scaleFactor: 3.5,
-        origin: { x: 461.7571105957031, y: 422.7299499511719 },
-        rotation: -2.1622128102625267,
+        origin: { x: 461.757, y: 422.73 },
+        rotation: -2.1622,
         offset: { x: -78, y: -40 },
         scaleY: -1,
         fillStyle: 'rgba(255, 255, 255, 1)'
     },
     right: {
         scaleFactor: 3.5,
-        origin: { x: 424.24277114868164, y: 422.72998046875 },
-        rotation: -0.9793798433272664,
+        origin: { x: 424.243, y: 422.73 },
+        rotation: -0.9794,
         offset: { x: -40, y: -40 },
         fillStyle: 'rgba(255, 255, 255, 1)'
     }
