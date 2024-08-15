f80d54c4965ffdecbdc45afd9c8377abf9ad359a
Vaulkryn <vaulkryn@outlook.fr>
2024-08-15 21:30:05 +0200

    updated for future better data automation

--- src/app/PartsLoader.js
+++ src/app/PartsLoader.js
@@ -1,26 +1,25 @@
 class PartsLoader {
-    constructor(coordsLoader, config, entity) {
+    constructor(coordsLoader, config, entityName, entityObject) {
         this.coordsLoader = coordsLoader;
-        this.entity = entity;
+        this.entityName = entityName;
+        this.entityObject = entityObject;
         this.config = config;
         this.parts = {};
     }
 
     async loadParts() {
-        const partsToLoad = this.coordsLoader.getAvailableCoords(this.entity);
+        const partsToLoad = this.coordsLoader.getAvailableCoords(this.entityName);
         for (const part of partsToLoad) {
-            this.parts[part] = this.coordsLoader.getCoords(this.entity, part);
+            this.parts[part] = this.coordsLoader.getCoords(this.entityName, part);
         }
     }
 
     addParts(part) {
-        this.parts[part] = this.coordsLoader.getCoords(this.entity, part);
-        //console.log(`Ajouté ${part}:`, this.parts[part]);
+        this.parts[part] = this.coordsLoader.getCoords(this.entityName, part);
     }
 
     deleteParts(part) {
         delete this.parts[part];
-        //console.log(`Supprimé ${part}`);
     }
 
     calculateCentroid(vertex) {
@@ -32,14 +31,18 @@ class PartsLoader {
     entityAssembler(ctx) {
         const centroid = this.calculateCentroid(this.parts.core);
 
+        //this.entityObject.polygons = [];
+
         for (const part in this.parts) {
             if (this.parts.hasOwnProperty(part)) {
                 this.partsFactory(ctx, this.parts[part], this.config[part], centroid);
+                // ici envoyer le tableau de coordonnées à une fonction qui transposera sous une forme que SAT.js accepte,
+                // puis envoyer le résultat à this.entityObject.polygons.
             }
         }
     }
 
-    partsFactory(ctx, vertex, config = {}, centroid) {
+    shapeParameters(config = {}, centroid) {
         const origin = config.origin || { x: 0, y: 0 };
         const rotation = config.rotation || 0;
         const offsetX = config.offset?.x || 0;
@@ -50,19 +53,51 @@ class PartsLoader {
         const correctedOffsetX = origin.x - this.config.core.origin.x + offsetX - centroid.x;
         const correctedOffsetY = origin.y - this.config.core.origin.y + offsetY - centroid.y;
 
+        return {
+            correctedOffsetX,
+            correctedOffsetY,
+            rotation,
+            scaleY,
+            scaleFactor
+        };
+    }
+
+    transformVertices(dot, params) {
+        let x = (dot.x + params.correctedOffsetX) / params.scaleFactor;
+        let y = (dot.y + params.correctedOffsetY) / params.scaleFactor;
+        y *= params.scaleY;
+
+        const cosTheta = Math.cos(params.rotation);
+        const sinTheta = Math.sin(params.rotation);
+
+        const rotatedX = x * cosTheta - y * sinTheta;
+        const rotatedY = x * sinTheta + y * cosTheta;
+
+        return { x: rotatedX, y: rotatedY };
+    }
+
+    partsFactory(ctx, vertex, config = {}, centroid) {
+        const params = this.shapeParameters(config, centroid);
         ctx.save();
-        ctx.translate(0, 0);
-        ctx.rotate(rotation);
-        ctx.scale(1, scaleY);
         ctx.beginPath();
-        ctx.moveTo((vertex[0].x + correctedOffsetX) / scaleFactor, (vertex[0].y + correctedOffsetY) / scaleFactor);
+        const newVertex = this.transformVertices(vertex[0], params);
+        ctx.moveTo(newVertex.x, newVertex.y);
         vertex.forEach(dot => {
-            ctx.lineTo((dot.x + correctedOffsetX) / scaleFactor, (dot.y + correctedOffsetY) / scaleFactor);
+            const transformedDot = this.transformVertices(dot, params);
+            ctx.lineTo(transformedDot.x, transformedDot.y);
         });
+
         ctx.closePath();
         ctx.fillStyle = config.fillStyle || 'rgba(0, 0, 0, 1)';
         ctx.fill();
         ctx.restore();
+    }
+
+    verticesFactory(vertex, config = {}, centroid) {
+        const params = this.shapeParameters(config, centroid);
+        const newVertex = vertex.map(dot => this.transformVertices(dot, params));
+        newVertex.push(newVertex[0]);
+        return newVertex;
     }
 }
 
